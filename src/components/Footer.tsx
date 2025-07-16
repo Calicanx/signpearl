f6dbe4de51a38d992016eb671d0f5bb3670a0bd7
Calicanx <rbnchris0@gmail.com>
2025-07-16 16:01:57 +0300

    chore: fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -62,7 +62,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 SignPearl. All rights reserved.</p>
+          <p>&copy; 2025 SignPearl. All rights reserved.</p>
         </div>
       </div>
     </footer>
